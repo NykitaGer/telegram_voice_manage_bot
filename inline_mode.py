97bcf6d26fd3adcd9a3ef485054ef42595e0d951
NykitaGer <2006nikitager@gmail.com>
2024-11-27 00:04:40 +0100

    add functions to get/save voice messages

---
+++ inline_mode.py
@@ -0,0 +1,19 @@
+from aiogram import Router, F
+from aiogram.types import (
+    InlineQuery,
+    InputTextMessageContent,
+    InlineQueryResultArticle
+)
+
+from aiogram.fsm.context import FSMContext
+
+router = Router()
+
+
+def get_voice_messages() -> list[str] | None:
+    return 
+
+
+@router.inline_query()
+async def inline_voice_message(inline_query: InlineQuery):
+    return
--- main.py
+++ main.py
@@ -1,4 +1,29 @@
 import asyncio
+import logging
+import os
 
 from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram import F
+from dotenv import load_dotenv
 
+load_dotenv()
+API_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=API_TOKEN, prase_mode=ParseMode.HTML)
+
+MEDIA_DIR = "saved_media"
+os.makedirs(MEDIA_DIR, exist_ok=True)
+
+async def main() -> None:
+    dp = Dispatcher()
+
+
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
