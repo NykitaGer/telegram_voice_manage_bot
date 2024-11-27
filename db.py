52faa4745dae8809206fc331f8f5dc4a217a13ef
NykitaGer <2006nikitager@gmail.com>
2024-11-27 19:04:13 +0100

    new function inline video message (beta)

--- db.py
+++ db.py
@@ -11,6 +11,15 @@ def create_tables() -> None:
     )"""
 
     cursor.execute(create_voice_table)
+
+    create_video_table = """CREATE TABLE IF NOT EXISTS videos (
+    id BIGINT,
+    name TEXT
+    )"""
+
+    cursor.execute(create_video_table)
+
+    db.commit()
     return
 
 def get_voices_from_db():
@@ -26,8 +35,20 @@ def add_voice_to_db(file_id: int, file_name: str) -> None:
     print(data)
     cursor.execute(insert_vioce_file, data)
     db.commit()
-    return
-
 
 def delete_voice_from_db():
     pass
+
+def get_videos_from_db():
+    get_videos = """SELECT id, name FROM videos"""
+    data = cursor.execute(get_videos).fetchall()
+    print(data)
+    return data
+
+def add_video_to_db(file_id: int, file_name: std):
+    insert_video_file = """INSERT INTO videos VALUES (?, ?)"""
+    data = (file_id, file_name)
+
+    print(data)
+    cursor.execute(insert_video_file, data)
+    db.commit()
--- inline_mode.py
+++ inline_mode.py
@@ -17,17 +17,26 @@ async def inline_voice_message(inline_query: InlineQuery):
     voices = get_voices_from_db()
 
     results = []
-    inline_query
+    
     for index, file_id, name in enumerate(voices):
-        # message_content = InputTextMessageContent(
-        #     message_text="" # TODO: here needs to send a voice message by its id
-        # )
         result_id = f"{index}-{inline_query.from_user.id}"
         results.append(InlineQueryResultAudio(
-            id=result_id, audio_url=file_id
+            id=result_id, title=name ,audio_url=file_id
         ))
-        # results.append(InlineQueryResultArticle(
-        #     id=result_id, title=name, input_message_content=message_content
-        # ))
+
+    await inline_query.answer(results, cache_time=0, is_personal=True)
+
+
+@router.inline_query()
+async def inline_video_message(inline_query: InlineQuery):
+    videos = 0 # TODO: create a function to get viodes from database
+
+    results = []
+
+    for index, file_id, name in enumerate(videos):
+        result_id = f"{index}-{inline_query.from_user.id}"
+        results.append(InlineQueryResultAudio(
+            id=result_id, title=name ,audio_url=file_id
+        ))
 
     await inline_query.answer(results, cache_time=0, is_personal=True)
