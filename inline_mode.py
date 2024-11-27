4ccca4bf3a150c3deab3b5e7dd75d81120e6592e
NykitaGer <2006nikitager@gmail.com>
2024-11-27 17:34:36 +0100

    new changes to inelin_voice_message function

--- inline_mode.py
+++ inline_mode.py
@@ -2,7 +2,8 @@ from aiogram import Router, F
 from aiogram.types import (
     InlineQuery,
     InputTextMessageContent,
-    InlineQueryResultArticle
+    InlineQueryResultArticle,
+    InlineQueryResultAudio
 )
 
 from aiogram.fsm.context import FSMContext
@@ -16,14 +17,17 @@ async def inline_voice_message(inline_query: InlineQuery):
     voices = get_voices_from_db()
 
     results = []
-
-    for index, id, name in enumerate(voices):
-        message_content = InputTextMessageContent(
-            message_text="" # TODO: here needs to send a voice message by its id
-        )
+    inline_query
+    for index, file_id, name in enumerate(voices):
+        # message_content = InputTextMessageContent(
+        #     message_text="" # TODO: here needs to send a voice message by its id
+        # )
         result_id = f"{index}-{inline_query.from_user.id}"
-        results.append(InlineQueryResultArticle(
-            id=result_id, title=name, input_message_content=message_content
+        results.append(InlineQueryResultAudio(
+            id=result_id, audio_url=file_id
         ))
+        # results.append(InlineQueryResultArticle(
+        #     id=result_id, title=name, input_message_content=message_content
+        # ))
 
     await inline_query.answer(results, cache_time=0, is_personal=True)
